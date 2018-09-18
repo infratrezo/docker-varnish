e1958ec789e84942e69736d35ae1a901e18e795e
Marco Aurélio Alano Godinho <marco@trezo.com.br>
2018-09-18 20:10:44 -0300

    Added backend port

--- 5.0.0/entrypoint.sh
+++ 5.0.0/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 HTTP_PORT=${HTTP_PORT:-6081}
 ADMIN_PORT=${ADMIN_PORT:-6082}
+BACKEND_PORT=${BACKEND_PORT:-80}
 
 if ! set | grep BACKEND ; then
     echo '$BACKEND variable is undefined. Please tell me the hostname/ip of the varnish backend'
@@ -8,6 +9,7 @@ if ! set | grep BACKEND ; then
 fi
 
 sed -i "s/^\(\s\|\t\)*\.host.*/    .host = \"$BACKEND\";/g" /etc/varnish/default.vcl
+sed -i "s/^\(\s\|\t\)*\.port.*/    .port = \"$BACKEND_PORT\";/g" /etc/varnish/default.vcl
 
 if varnishd -Cf /etc/varnish/default.vcl 1>/dev/null ; then
     exec /usr/sbin/varnishd -j unix,user=vcache -F -a :${HTTP_PORT} -T localhost:${ADMIN_PORT} -f /etc/varnish/default.vcl -S /etc/varnish/secret -s malloc,256m
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 # Usage
  * The BACKEND environment variable is replaced by the entrypoint script in the VCL
+ * The default BACKEND_PORT value is 80, but this can be replaced by setting the environment variable
  * You can optionally specify alternative HTTP_PORT and ADMIN_PORT - default values are 6081 and 6082
